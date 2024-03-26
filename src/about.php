c16d35c02acb5a4a31271307912309abe992dca2
Jean le Grand Bokassa <jean.le.grand.bokassa@gmail.com>
2024-03-26 14:41:57 +0100

    wip

--- src/about.php
+++ src/about.php
@@ -3,7 +3,7 @@
 <?php include 'includes/nav.php'; ?>
 <div class="container">
     <h1>À propos de ce blog</h1>
-    <p>Ce blog de basketball est une plateforme où nous partageons des informations, des analyses et des opinions sur le monde du basketball.</p>
+    <p>Ce blog de basketball est une plateforme où nous<br> partageons des informations, des analyses et des opinions<br> sur le monde du basketball.</p>
 </div>
 </body>
 </html>
